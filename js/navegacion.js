de3626af70fe222b46e44a7486009ca4368a96a0
hernandezmery95 <hernandezmery95@gmail.com>
2024-06-20 19:31:17 -0300

    navegacion con javascript

---
+++ js/navegacion.js
@@ -0,0 +1,32 @@
+const body = document.body
+const header = document.createElement('header');
+body.appendChild(header)
+
+const nav = document.createElement('nav');
+const ul = document.createElement('ul');
+const links = [
+    {
+        label: "Inicio",
+        file: "index.html"
+    }, 
+    {
+        label: "Productos",
+        file: "products.html"
+    },
+    {
+        label: "Contacto",
+        file: "contact.html"
+    },
+]
+
+header.appendChild(nav);
+nav.appendChild(ul)
+
+for (const link of links) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = link.file;
+    a.textContent = link.label;
+    li.appendChild(a);
+    ul.appendChild(li);
+}
